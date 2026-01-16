0cbf7a46cba11d15ca1476989a83432d23480681
JimPalo <jim.palo@elev.ga.ntig.se>
2026-01-16 16:45:34 +0100

    Headers till Params

--- handout/1 - example_requests/lib/request.rb
+++ handout/1 - example_requests/lib/request.rb
@@ -1,10 +1,11 @@
 class Request
-  attr_reader :method, :resource, :version
+  attr_reader :method, :resource, :version, :headers, :params
   def initialize(request_string)
     @request_string = request_string
+    @params = {}
     parse_request_line
     parse_headers
-
+    parse_params
   end
 
   def parse_request_line
@@ -16,11 +17,46 @@ class Request
   def parse_headers
     @headers = {}
     lines = @request_string.split(/\r?\n/)
-    header_lines = lines[1]
+    request_line, *header_lines = lines #divide up the first row with the rest
     header_lines.each do |line|
-    if line.strip.empty?
-      break
+      if line.strip.empty?
+        break
+        else
+          key, value = line.split(": ", 2)
+          @headers[key] = value
+      end
     end
+  end
+
+  def parse_params
+    parse_get_params
+    parse_post_params
+  end
+
+  def parse_get_params
+      if @resource.include?("?")
+        path, query_string = @resource.split("?", 2)
+        @resource = path
+        query_string.split("&").each do |pair| #key-value pair
+          key, value = pair.split("=", 2)
+          @params[key] = value
+        end
+    end
+  end
+
+  def parse_post_params
+    if @method == "POST"
+      body = @request_string.split(/\r?\n\r?\n/, 2)[1]
+      body.split("&").each do |pair|
+        key, value = pair.split("=", 2)
+        @params[key] = value.strip
+      end
+    else return
+    end
+
+  end
+  
+  
 
 
 end
--- handout/1 - example_requests/test/request_test.rb
+++ handout/1 - example_requests/test/request_test.rb
@@ -25,16 +25,35 @@ class RequestTest < Minitest::Test
   end
 
   def test_parses_headers_from_simple_get
-    request_string = File.read('./getindex.request.txt')
+    request_string = File.read('../get-index.request.txt')
     request = Request.new(request_string)
+    expected_headers =
+      {'Host' => 'developer.mozilla.org', 
+      'Accept-Language' => 'fr'}
+    
+  
 
-    assert_equal "{'Host' => 'developer.mozilla.org', 'Accept-Language' => 'fr'}", request.headers
+    assert_equal expected_headers, request.headers
 
   end
 
+  def test_parses_params_from_simple_get
+    request_string = File.read('../get-fruits-with-filter.request.txt')
+    request = Request.new(request_string)
+    expected_params = {
+      'type' => 'bananas', 'minrating' => '4'
+    }
+    assert_equal expected_params, request.params
+  end
 
-
-
+  def test_parses_params_from_simple_post
+    request_string = File.read('../post-login.request.txt')
+    request = Request.new(request_string)
+    expected_params = {
+      'username' => 'grillkorv', 'password' => 'verys3cret!'
+    }
+    assert_equal expected_params, request.params
+  end
   
 
 end
